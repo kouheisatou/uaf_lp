6af02d9c7b731f870d7ff5239fc1383d0714c57a
kouheisatou <koheisato0420@gmail.com>
2025-06-12 23:02:13 +0900

    auto deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/uaf_lp/",
   plugins: [react()],
-  server: {
-    open: true,
-  },
 }); 
